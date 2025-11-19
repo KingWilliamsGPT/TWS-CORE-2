f2201f08b469b20ae44363cfc12d57a35f5f3de3
Williams Samuel <williamusanga22@gmail.com>
2025-11-19 19:44:43 +0100

    Update STATIC_ROOT path in common.py to use 'static' directory for static files

--- src/config/common.py
+++ src/config/common.py
@@ -325,7 +325,7 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Where files are collected
+STATIC_ROOT = os.path.join(BASE_DIR, "static")  # Where files are collected
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # Your additional static directories
 ]
